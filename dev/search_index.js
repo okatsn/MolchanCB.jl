9a54c6eb005a852d0345827770092deaf45e54fb
Documenter.jl <documenter@juliadocs.github.io>
2023-11-14 08:53:41 +0000

    build based on 6772b8d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MolchanCB","category":"page"},{"location":"#MolchanCB","page":"Home","title":"MolchanCB","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MolchanCB.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MolchanCB]","category":"page"}]
+}
